e010998ff57fed2c409aeda8021bd9dc2bec0113
Talent Zeng <talent.z@hotmail.com>
2018-11-11 21:07:29 -0500

    added basic requirements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png" className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import RootReducer from './reducers/RootReducer';
+
+export default function configureStore(initialState={}) {
+ return createStore(
+   RootReducer,
+   applyMiddleware(thunk)
+ );
+}
